428b8e69b606fb5938df674c80c83bbe02f0fff2
Michael Villena <mvillena3@gatech.edu>
2012-05-01 22:52:54 -0400

    1st commit

---
+++ Anime-Rec/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnimeRec::Application.config.secret_token = '757ce42527200a9344fc8a5ece500ca967d4439ca61f3e555116d07293572854ea4ef8529ca4004ff7bdf2cec542690bcc9c075ef18bf413dc07e12b3ebb9c5f'
---
+++ README.md
@@ -0,0 +1,2 @@
+**Sup, Welcome to Anime Rec**
+It is teh greatness of nothing.:q
