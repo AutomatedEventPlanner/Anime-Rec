0d052cd000f04c1ff75591886f4622f2120e4cb2
Michael Villena <mvillena3@gatech.edu>
2012-05-05 18:50:37 -0400

    Interests column added to user db

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,17 @@
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime        not null
-#  updated_at :datetime        not null
+#  id              :integer         not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime        not null
+#  updated_at      :datetime        not null
+#  password_digest :string(255)
+#  interests       :text
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :password, :password_confirmation
+  attr_accessible :email, :name, :password, :password_confirmation, :interests
 
   has_secure_password
 
