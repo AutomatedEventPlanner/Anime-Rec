56d4c4ff31dfa1421016c34f8a3f736f74f70611
Michael Villena <mvillena3@gatech.edu>
2012-05-05 14:51:26 -0400

    Important uniqueness update

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,5 +14,6 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true, length: { maximum: 50 }
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: EMAIL_REGEX }
+  validates :email, presence: true, format: { with: EMAIL_REGEX }, 
+    uniqueness: true
 end
