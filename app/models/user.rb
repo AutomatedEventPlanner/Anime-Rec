9549a3a2e89b0bc588c67db34a9940d4ea2ae03d
Michael Villena <mvillena3@gatech.edu>
2012-05-05 15:13:28 -0400

    Email uniqueness update with an email index

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,11 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
 
+  before_save { |user| user.email = email.downcase }
+
   validates :name, presence: true, length: { maximum: 50 }
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: EMAIL_REGEX }, 
-    uniqueness: true
+    uniqueness: { case_sensitive: false }
+
 end
